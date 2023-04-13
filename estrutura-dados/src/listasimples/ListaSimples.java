fc062606774d106a7ac1a83fa6ed82beb844fae4
João Gabriel Barbosa <joaogcb29@gmail.com>
2023-04-13 16:45:23 -0300

    Refactoring

--- estrutura-dados/src/listasimples/ListaSimples.java
+++ estrutura-dados/src/listasimples/ListaSimples.java
@@ -2,93 +2,79 @@ package listasimples;
 
 class ListaSimples {
 
-    private No primeiro;
-    private No ultimo;
-    private int numeroNos;
+    private No primeiro, ultimo;
 
     ListaSimples() {
-        this.primeiro = null;
-        this.ultimo = null;
-        this.numeroNos = 0;
+        this.primeiro = this.ultimo = null;
     }
 
     void insereNoInicio(No novoNo) {
-        if (this.primeiro == null) {
-            this.primeiro = novoNo;
-            this.ultimo = novoNo;
-            novoNo.proximo = null;
-        } else {
-            novoNo.proximo = primeiro;
-            this.primeiro = novoNo;
-        }
+        novoNo.proximo = this.primeiro;
+        this.primeiro = novoNo;
     }
 
     void insereNoFim(No novoNo) {
-        if (this.primeiro == null) {
-            this.primeiro = novoNo;
-            this.ultimo = novoNo;
-            novoNo.proximo = null;
-        } else if (this.ultimo != null) {
-            novoNo.proximo = null;
-            this.ultimo.proximo = novoNo;
-            this.ultimo = novoNo;
+        No noAuxiliar = this.primeiro;
+        while (noAuxiliar.proximo != null) {
+            noAuxiliar = noAuxiliar.proximo;
         }
+        novoNo.proximo = null;
+        noAuxiliar.proximo = novoNo;
+        this.ultimo = novoNo;
     }
 
-    void insereNoPosicao(No novoNo, int posicao) {
-        No noAuxiliar = primeiro;
-        int numeroNos = this.contarNos();
-        int posAux;
-        if (posicao == 0) {
-            novoNo.proximo = primeiro;
-            primeiro = novoNo;
-            if (primeiro == ultimo) {
-                ultimo = null;
+    void insereNoPosicao(int posicao, No novoNo) {
+        No noAuxiliar = this.primeiro;
+        No noAuxiliarAnterior = this.primeiro;
+        int tamanho = 0;
+        if (posicao >= 0) {
+            if (posicao == 0) {
+                this.insereNoInicio(novoNo);
+                return;
+            } else if (posicao == this.contarNos()) {
+                this.insereNoFim(novoNo);
+                return;
             }
-        } else if (posicao <= numeroNos) {
-            posAux = 1;
-            while (noAuxiliar != null && posicao > posAux) {
+            while (this.contarNos() > posicao) {
+                if (tamanho == posicao) {
+                    noAuxiliarAnterior.proximo = novoNo;
+                    novoNo.proximo = noAuxiliar;
+                    return;
+                }
+                noAuxiliarAnterior = noAuxiliar;
                 noAuxiliar = noAuxiliar.proximo;
-                posAux++;
+                tamanho++;
             }
-            novoNo.proximo = noAuxiliar.proximo;
-            noAuxiliar.proximo = novoNo;
-        } else if (posicao > numeroNos) {
-            ultimo.proximo = novoNo;
-            ultimo = novoNo;
         }
-
     }
 
-    String buscaNo(int valor) {
-        No noAuxiliar = primeiro;
-        int posicao = 0;
+    No buscaNo(int valor) {
+        No noAuxiliar = this.primeiro;
         while (noAuxiliar != null) {
             if (noAuxiliar.valor == valor) {
-                return "Nó " + noAuxiliar.valor + " na posição " + posicao;
+                return noAuxiliar;
             }
             noAuxiliar = noAuxiliar.proximo;
-            posicao++;
         }
         return null;
     }
 
     void excluiNo(int valor) {
-        No noAuxiliar = primeiro;
-        No noAuxiliarAnterior;
+        No noAuxiliar = this.primeiro;
+        No noAuxiliarAnterior = this.primeiro;
         while (noAuxiliar != null) {
-            noAuxiliarAnterior = noAuxiliar;
-            noAuxiliar = noAuxiliar.proximo;
             if (noAuxiliar.valor == valor) {
                 noAuxiliarAnterior.proximo = noAuxiliar.proximo;
                 return;
             }
+            noAuxiliarAnterior = noAuxiliar;
+            noAuxiliar = noAuxiliar.proximo;
         }
     }
 
     int contarNos() {
+        No noAuxiliar = this.primeiro;
         int tamanho = 0;
-        No noAuxiliar = primeiro;
         while (noAuxiliar != null) {
             tamanho++;
             noAuxiliar = noAuxiliar.proximo;
@@ -96,14 +82,17 @@ class ListaSimples {
         return tamanho;
     }
 
-    void exibeLista(){
-        No noAuxiliar = primeiro;
-        int i = 0;
-        while(noAuxiliar != null) {
-            System.out.println("Valor " + noAuxiliar.valor + " posição " + i);
-            noAuxiliar = noAuxiliar.proximo;
-            i++;
+    String exibeLista() {
+        if (this.contarNos() != 0) {
+            String lista = "";
+            No noAuxiliar = this.primeiro;
+            while (noAuxiliar != null) {
+                lista += "Nó " + noAuxiliar.valor + " --> ";
+                noAuxiliar = noAuxiliar.proximo;
+            }
+            return lista;
         }
+        return "A lista está vazia.";
     }
 
 }
--- estrutura-dados/src/listasimples/Main.java
+++ estrutura-dados/src/listasimples/Main.java
@@ -6,22 +6,41 @@ class Main {
 
         ListaSimples minhaLista = new ListaSimples();
 
-        minhaLista.insereNoFim(new No(1));
-        minhaLista.insereNoFim(new No(2));
-        minhaLista.insereNoFim(new No(5));
-        minhaLista.insereNoPosicao(new No(4), 2);
-        minhaLista.insereNoPosicao(new No(0), 0);
-        minhaLista.insereNoPosicao(new No(3), 2);
+        minhaLista.insereNoInicio(new No(1));
+        System.out.println(minhaLista.exibeLista());
 
-        minhaLista.exibeLista();
+        minhaLista.insereNoInicio(new No(2));
+        minhaLista.insereNoInicio(new No(3));
+        System.out.println(minhaLista.exibeLista());
+
+        minhaLista.insereNoFim(new No(10));
+        System.out.println(minhaLista.exibeLista());
+        System.out.println(minhaLista.contarNos());
 
         minhaLista.excluiNo(2);
-        System.out.println();
-        minhaLista.exibeLista();
-
         minhaLista.excluiNo(5);
-        System.out.println();
-        minhaLista.exibeLista();
+        System.out.println(minhaLista.buscaNo(3));
+        System.out.println(minhaLista.exibeLista());
+        System.out.println(minhaLista.contarNos());
+
+        minhaLista.insereNoPosicao(2, new No(9));
+        System.out.println(minhaLista.exibeLista());
+        System.out.println(minhaLista.contarNos());
+
+        minhaLista.insereNoPosicao(6, new No(12));
+        System.out.println(minhaLista.exibeLista());
+
+        minhaLista.insereNoPosicao(0, new No(12));
+        System.out.println(minhaLista.exibeLista());
+
+        minhaLista.insereNoPosicao(4, new No(15));
+        System.out.println(minhaLista.exibeLista());
+
+        minhaLista.insereNoPosicao(6, new No(20));
+        System.out.println(minhaLista.exibeLista());
+
+        minhaLista.insereNoPosicao(-12, new No(50));
+        System.out.println(minhaLista.exibeLista());
 
     }
 
